abe7631d2c3aedbce5c0e84938a56008545821fb
reksuat <pyea08@gmail.com>
2026-03-14 10:18:56 -0300

    add chance imagem hover

--- interative.js
+++ interative.js
@@ -8,3 +8,13 @@ function toggleTheme() {
         document.getElementById("themeToggle").textContent = "☾";
     }
 }
+function toggleImage(img) {
+  if (!img.dataset.original) {
+        img.dataset.original = img.src;
+    }
+    if (img.src === img.dataset.original) {
+        img.src = img.dataset.hover;
+    } else {
+        img.src = img.dataset.original;
+    }
+}
