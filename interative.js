fac209018c28d3c7f493414a9340bd9488baef7f
reksuat <pyea08@gmail.com>
2026-03-17 15:46:30 -0300

    feat mensagem form

--- interative.js
+++ interative.js
@@ -18,3 +18,15 @@ function toggleImage(img) {
         img.src = img.dataset.original;
     }
 }
+const formReserva = document.getElementById('formReserva');
+const msgReserva = document.getElementById('mensagemReserva');
+const nomeCliente = document.getElementById('nome');
+
+formReserva.addEventListener('submit', function(event) {
+    event.preventDefault(); 
+
+    const nome = document.getElementById('name').value;
+    nomeCliente.textContent = nome;
+    msgReserva.style.display = 'block';
+    formReserva.reset();
+});
